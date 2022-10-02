8f8a351a3837b7d450e12b65bc29a7d5b3af123d
sparklingwaterlemon <michaelkim009@gmail.com>
2022-10-02 15:55:39 -0700

    movie react lab - setting up - npm installed necessary nodes - have react and express running and communicating - setting up skeleton page level componenets

---
+++ react-movie-lab/src/pages/App/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+import './App.css';
+
+function App() {
+  const [user, setUser] = useState(null);
+
+  return (
+    <main className="App">
+      <h1> Test App.js </h1>
+    </main>
+  );
+}
+
+export default App;
