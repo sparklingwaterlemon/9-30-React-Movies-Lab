6063f89ade31c8507f7410a58aa82e812098fc9d
sparklingwaterlemon <michaelkim009@gmail.com>
2022-10-02 23:12:32 -0700

    displayed movie poster, title, and release date in MovieCardPage component

--- react-movie-lab/src/pages/MovieIndexPage/MovieCardPage/MovieCardPage.jsx
+++ react-movie-lab/src/pages/MovieIndexPage/MovieCardPage/MovieCardPage.jsx
@@ -1,26 +1,31 @@
 import "./MovieCardPage.css";
+import {useState} from "react";
 import {movies} from "../../../public/data.js"
 
 
+
 function MovieCardPage(){
+    const movieList = movies.map((element) => {
+        console.log(element)
+        return (
+            <>
+                <img className="movie-poster" src={`${element.poster_path}`} />
+                <h1 className="movie-poster-title"> {element.title} </h1>
+                <h2 className="movie-poster-release-date"> {element.release_date} </h2>
+            </>
+        )
+    })
+
+
     return(
-        <h1 className="movie-card-title"> Movie Card. jsx </h1>
+        <>
+            <h1 className="movie-card-title"> Movie Card. jsx </h1>
+            <div className="movie-card">
+                {movieList}
+            </div>
+        </>
     )
 }
 
 
 export default MovieCardPage
-
-// const movies = [
-//     {
-//       "poster_path": "https://image.tmdb.org/t/p/w500/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg",
-//       "release_date": "2020-12-16",
-//       "title": "Wonder Woman 1984",
-//       "cast": [
-//         "Gal Gadot",
-//         "Pedro Pascal",
-//         "Chris Pine",
-//         "Kristen Wiig",
-//         "Robin Wright"
-//       ]
-//     },
